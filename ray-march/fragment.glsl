4eb706b7bce204c59a300758c06d6d8c404b2eda
Jonas Luebbers <jluebbers@squarespace.com>
2017-05-14 18:11:58 -0400

    Add sphere function

--- ray-march/fragment.glsl
+++ ray-march/fragment.glsl
@@ -3,8 +3,21 @@ precision mediump float;
 uniform mat4 viewMatrix;
 uniform vec2 resolution;
 
-vec3 directLight(in vec3 origin, in vec3 direction) {
-	return vec3(0.0, 0.0, 1.0);
+bool sphere(in vec3 ro, in vec3 rd, in vec3 p, in float l) {
+	float a = dot(rd, rd);
+	float b = dot(rd, ro - p) * 2.0;
+	float c = dot(p, p) + dot(ro, ro) - (2.0 * dot(p, ro)) - (l * l);
+	float discriminant = (b * b) - (4.0 * a * c);
+	return discriminant > 0.0;
+}
+
+vec3 directLight(in vec3 ro, in vec3 rd) {
+	if (sphere(ro, rd, vec3(0.0, 0.0, 10.0), 2.0)) {
+		return vec3(1.0, 0.0, 0.0);
+	}
+	else {
+		return vec3(0.0, 0.0, 1.0);
+	}
 }
 
 void main() {
