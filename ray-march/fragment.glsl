2de3ec5b22e9b12cbed90a2236187bf504c351ae
Jonas Luebbers <jluebbers@squarespace.com>
2017-05-17 01:42:42 -0400

    Refactor to use structs for common data structures

--- ray-march/fragment.glsl
+++ ray-march/fragment.glsl
@@ -3,41 +3,41 @@ precision mediump float;
 uniform mat4 viewMatrix;
 uniform vec2 resolution;
 
-const float kd = 0.99;
-const float ka = 0.2;
+struct Sphere {
+	vec3 p;
+	float r;
+};
 
-const float MAX_DISTANCE = 1000.0;
+struct Light {
+	vec3 p;
+	float kd;
+	float ka;
+};
 
-const vec3 LIGHT_CENTER = vec3(10.0, 10.0, 5.0);
-const vec3 SPHERE_CENTER = vec3(0.0, 0.0, 10.0);
-const float SPHERE_RADIUS = 2.0;
+struct Ray {
+	vec3 p;
+	vec3 d;
+};
 
-float sphereDistance(in vec3 ro, in vec3 rd, in vec3 p, in float l) {
-	float a = dot(rd, rd);
-	float b = dot(rd, ro - p) * 2.0;
-	float c = dot(p, p) + dot(ro, ro) - (2.0 * dot(p, ro)) - (l * l);
+const Light L = Light(vec3(10.0, 10.0, 5.0), 0.99, 0.2);
+const Sphere A = Sphere(vec3(0.0, 0.0, 10.0), 2.0);
+
+vec3 traceRay(in Ray R) {
+	float a = dot(R.d, R.d);
+	float b = dot(R.d, R.p - A.p) * 2.0;
+	float c = dot(A.p, A.p) + dot(R.p, R.p) - (2.0 * dot(A.p, R.p)) - (A.r * A.r);
+
 	float d = (b * b) - (4.0 * a * c);
-	if (d < 0.0) {
-		return -1.0;
-	}
-	else {
+	if (d >= 0.0) {
 		float t = (-b - sqrt(d)) / 2.0 * a;
-		return t;
-	}
-}
-
-vec3 directLight(in vec3 ro, in vec3 rd) {
-	float t = sphereDistance(ro, rd, SPHERE_CENTER, SPHERE_RADIUS);
-	if (t < 0.0) {
-		return vec3(0.0, 0.0, 1.0);
+		vec3 Q = R.p + (t * R.d);
+		vec3 N = (Q - A.p) / A.r;
+		vec3 D = normalize(L.p - Q);
+		float e = dot(N, D);
+		return vec3((L.kd * e) + L.ka);
 	}
 	else {
-		vec3 Q = ro + (t * rd);
-		vec3 N = (Q - SPHERE_CENTER) / SPHERE_RADIUS;
-		vec3 L = normalize(LIGHT_CENTER - Q);
-		float a = dot(N, L);
-		vec3 c = vec3((kd * a) + ka);
-		return c;
+		return vec3(0.0, 0.0, 1.0);
 	}
 }
 
@@ -45,6 +45,7 @@ void main() {
 	float scaling = min(resolution.x, resolution.y);
 	vec3 origin = vec3(0.0, 0.0, 0.0);
 	vec3 direction = normalize(vec3((gl_FragCoord.xy - (resolution / 2.0)) / scaling, 1.0));
-	vec3 light = directLight(origin, direction);
+	Ray R = Ray(origin, direction);
+	vec3 light = traceRay(R);
 	gl_FragColor = vec4(light, 1.0);
 }
