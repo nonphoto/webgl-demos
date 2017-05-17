5472d1b749d6bd1c69f7350169d5ed9b19cfd860
Jonas Luebbers <jluebbers@squarespace.com>
2017-05-17 01:20:11 -0400

    Fix coordinate scaling and add blue background

--- ray-march/fragment.glsl
+++ ray-march/fragment.glsl
@@ -3,10 +3,12 @@ precision mediump float;
 uniform mat4 viewMatrix;
 uniform vec2 resolution;
 
-const float kd = 0.9;
-const float ka = 0.1;
+const float kd = 0.99;
+const float ka = 0.2;
 
-const vec3 LIGHT_CENTER = vec3 (0.0, 10.0, 10.);
+const float MAX_DISTANCE = 1000.0;
+
+const vec3 LIGHT_CENTER = vec3(10.0, 10.0, 5.0);
 const vec3 SPHERE_CENTER = vec3(0.0, 0.0, 10.0);
 const float SPHERE_RADIUS = 2.0;
 
@@ -14,23 +16,35 @@ float sphereDistance(in vec3 ro, in vec3 rd, in vec3 p, in float l) {
 	float a = dot(rd, rd);
 	float b = dot(rd, ro - p) * 2.0;
 	float c = dot(p, p) + dot(ro, ro) - (2.0 * dot(p, ro)) - (l * l);
-	float t = (-b - sqrt((b * b) - (4.0 * a * c))) / 2.0 * a;
-	return t;
+	float d = (b * b) - (4.0 * a * c);
+	if (d < 0.0) {
+		return -1.0;
+	}
+	else {
+		float t = (-b - sqrt(d)) / 2.0 * a;
+		return t;
+	}
 }
 
 vec3 directLight(in vec3 ro, in vec3 rd) {
 	float t = sphereDistance(ro, rd, SPHERE_CENTER, SPHERE_RADIUS);
-	vec3 Q = ro + (t * rd);
-	vec3 N = (Q - SPHERE_CENTER) / SPHERE_RADIUS;
-	vec3 L = normalize(LIGHT_CENTER - Q);
-	float a = dot(N, L);
-	vec3 c = vec3((kd * a) + ka);
-	return c;
+	if (t < 0.0) {
+		return vec3(0.0, 0.0, 1.0);
+	}
+	else {
+		vec3 Q = ro + (t * rd);
+		vec3 N = (Q - SPHERE_CENTER) / SPHERE_RADIUS;
+		vec3 L = normalize(LIGHT_CENTER - Q);
+		float a = dot(N, L);
+		vec3 c = vec3((kd * a) + ka);
+		return c;
+	}
 }
 
 void main() {
+	float scaling = min(resolution.x, resolution.y);
 	vec3 origin = vec3(0.0, 0.0, 0.0);
-	vec3 direction = normalize(vec3((gl_FragCoord.xy / resolution - 0.5) * 2.0, 1.0));
+	vec3 direction = normalize(vec3((gl_FragCoord.xy - (resolution / 2.0)) / scaling, 1.0));
 	vec3 light = directLight(origin, direction);
 	gl_FragColor = vec4(light, 1.0);
 }
